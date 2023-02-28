a7f92b232ed794564a7daed4dc25c5d7698ed873
talhasheikh041 <124531550+talhasheikh041@users.noreply.github.com>
2023-03-01 02:53:17 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,108 @@
+const movieInput = document.querySelector("#movie-input");
+const searchBtn = document.querySelector("#search-btn");
+const searchList = document.querySelector("#search-list");
+const startExploringDiv = document.querySelector("#start-exploring-div");
+
+let tempArr = [];
+
+const watchListArray = JSON.parse(localStorage.getItem("watchlist"))
+  ? JSON.parse(localStorage.getItem("watchlist"))
+  : [];
+
+movieInput.addEventListener("keydown", function (e) {
+  if (e.type === "click" || (e.type === "keydown" && e.keyCode === 13)) {
+    fetchMovieData();
+  }
+});
+searchBtn.addEventListener("click", function (e) {
+  if (e.type === "click" || (e.type === "keydown" && e.keyCode === 13)) {
+    fetchMovieData();
+  }
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target.id === "add-movie-btn") {
+    addMovieToWatchlist(e);
+  }
+});
+
+function addMovieToWatchlist(e) {
+  if (
+    watchListArray.findIndex(
+      (movie) => movie.Title === e.target.dataset.title
+    ) === -1
+  ) {
+    const addMovieObj = tempArr.find(
+      (movie) => movie.Title === e.target.dataset.title
+    );
+    watchListArray.push(addMovieObj);
+    localStorage.setItem("watchlist", JSON.stringify(watchListArray));
+    alert("Added to your watchlist!!");
+  } else {
+    alert("Already present in your watchlist..!");
+  }
+}
+
+async function fetchMovieData() {
+  const res = await fetch(
+    `http://www.omdbapi.com/?apikey=36660f2f&s=${movieInput.value}`
+  );
+  const searchData = await res.json();
+  setTimeout(() => {
+    generateMovieHtml(searchData.Response, searchData.Search);
+  }, 500);
+}
+
+function generateMovieHtml(response, searchArr) {
+  const data = [];
+  startExploringDiv.style.display = "none";
+
+  if (response === "True") {
+    searchArr.forEach((item) => {
+      fetch(`http://www.omdbapi.com/?apikey=36660f2f&i=${item.imdbID}`)
+        .then((res) => res.json())
+        .then((updatedData) => {
+          data.push(updatedData);
+        });
+    });
+    searchList.innerHTML = `<div class="lds-dual-ring"></div>`;
+    setTimeout(() => {
+      tempArr = [...data];
+      searchList.innerHTML = data
+        .map((movie) => {
+          return `
+            <div class="movie">
+            <div class="img-container">
+                <img class="movie-img" src="${movie.Poster}" alt="">
+            </div>
+            <div class="description-container">
+                <div class="title">
+                    <h2 class="movie-title">${movie.Title}</h2>
+                    <p class="movie-year">(${movie.Year})</p>
+                    <img src="images/star-icon.png" alt="" class="star-icon">
+                    <p class="movie-rating">${movie.imdbRating}</p>
+                </div>
+                <div class="details">
+                    <p class="movie-timing">${movie.Runtime}</p>
+                    <p class="movie-genre">${movie.Genre}</p>
+                    <div class="add-btn-div">
+                        <img src="images/add-icon.png" alt="">
+                        <span data-title="${movie.Title}" id="add-movie-btn" >Watchlist</span>
+                    </div>
+                </div>
+                <p class="movie-description">${movie.Plot}</p>
+            </div>
+        </div>
+            `;
+        })
+        .join("");
+    }, 1500);
+  } else {
+    return `<h2 style="
+                text-align: center;
+                color: #787878;
+                margin-top: 30px;
+                ">
+                Unable to find what you’re looking for. Please try another search.</h2>`;
+  }
+}
---
+++ watchlist.js
@@ -0,0 +1,68 @@
+const movieList = document.querySelector("#movie-list");
+
+let movies = JSON.parse(localStorage.getItem("watchlist"))
+  ? JSON.parse(localStorage.getItem("watchlist"))
+  : [];
+
+document.addEventListener("click", function (e) {
+  if (e.target.id === "remove-movie-btn") {
+    removeFromWatchlist(e.target.dataset.title);
+    localStorage.setItem("watchlist", JSON.stringify(movies));
+    render();
+  }
+});
+
+function removeFromWatchlist(title) {
+  movies = movies.filter((movie) => {
+    return movie.Title !== title;
+  });
+}
+
+function generateWatchlistHtml() {
+  if (movies.length === 0) {
+    return `
+        <div id="start-searching-div" class="start-searching-div">
+        <h2>Your watchlist is looking a little empty...</h2>
+        <a href="index.html">
+            <img src="images/add-icon.png" alt="">
+            <span>Let’s add some movies!</span>
+        </a>
+    </div>
+        `;
+  }
+
+  return movies
+    .map((movie) => {
+      return `
+        <div class="movie">
+        <div class="img-container">
+            <img class="movie-img" src="${movie.Poster}" alt="">
+        </div>
+        <div class="description-container">
+            <div class="title">
+                <h2 class="movie-title">${movie.Title}</h2>
+                <p class="movie-year">(${movie.Year})</p>
+                <img src="images/star-icon.png" alt="" class="star-icon">
+                <p class="movie-rating">${movie.imdbRating}</p>
+            </div>
+            <div class="details">
+                <p class="movie-timing">${movie.Runtime}</p>
+                <p class="movie-genre">${movie.Genre}</p>
+                <div class="remove-btn-div">
+                    <img src="images/remove-icon.png" alt="">
+                    <span data-title="${movie.Title}" id="remove-movie-btn" >Remove</span>
+                </div>
+            </div>
+            <p class="movie-description">${movie.Plot}</p>
+        </div>
+    </div>
+        `;
+    })
+    .join("");
+}
+
+function render() {
+  movieList.innerHTML = generateWatchlistHtml();
+}
+
+render();
